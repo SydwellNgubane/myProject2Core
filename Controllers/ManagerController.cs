766f5c0f8264ff7b6f0cc30161a06a14325fbc4e
Lungelo Ngubane <sydwellngubane@gmail.com>
2020-11-17 16:18:07 +0200

    Add project files.

---
+++ Controllers/ManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTwoMVC.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult ManagerLogin()
+        {
+            return View();
+        }
+        public IActionResult ManagerIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/NormalController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTwoMVC.Controllers
+{
+    public class NormalController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult NormalIndex()
+        {
+            return View();
+        }
+        /*----------------------------------*/
+        public IActionResult NormalLogin()
+        {
+            return View();
+        }
+        /*----------------------------------*/
+    }
+}
